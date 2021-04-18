812f7665c5697dd3f6da94c7f2798b6e6ce2dcb6
KentaMaeda0916 <67748136+KentaMaeda0916@users.noreply.github.com>
2021-04-18 14:19:35 +0900

    Initial Commit

---
+++ CastomTabView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CastomTabView
+//
+//  Created by まえけん on 2021/04/18.
+//  Copyright © 2021 maeken. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
